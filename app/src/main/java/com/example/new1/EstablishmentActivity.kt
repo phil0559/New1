575b86ad320132093a344f948e3a636118fe1d73
phil0559 <phdesproges@aol.com>
2025-10-13 20:07:13 +0200

    Add back navigation arrow to establishment page

--- app/src/main/java/com/example/new1/EstablishmentActivity.kt
+++ app/src/main/java/com/example/new1/EstablishmentActivity.kt
@@ -1,11 +1,16 @@
 package com.example.new1
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class EstablishmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_establishment)
+    }
+
+    fun onBackArrowClicked(view: View) {
+        onBackPressedDispatcher.onBackPressed()
     }
 }
