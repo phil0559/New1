533427f3d1f6883c6a755f80fc9e15ca59dfa36a
phil0559 <phdesproges@aol.com>
2025-10-13 20:55:04 +0200

    Enable back navigation from establishment screen

--- app/src/main/java/com/example/new1/EstablishmentActivity.kt
+++ app/src/main/java/com/example/new1/EstablishmentActivity.kt
@@ -1,11 +1,15 @@
 package com.example.new1
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class EstablishmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_establishment)
+
+        val backButton: ImageView = findViewById(R.id.button_back)
+        backButton.setOnClickListener { finish() }
     }
 }
