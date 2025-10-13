7cb1f31957ecbe0d8e8b69c2fa91b99087b8cb04
phil0559 <phdesproges@aol.com>
2025-10-13 11:06:49 +0200

    Ajout de l'écran Établissement

---
+++ app/src/main/java/com/example/new1/EstablishmentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.new1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EstablishmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_establishment)
+    }
+}
