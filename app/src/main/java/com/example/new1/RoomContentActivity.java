9d2e3fb10991afe170a5b8cedceaee55f9ed6a3e
phil0559 <phdesproges@aol.com>
2025-10-17 16:39:08 +0200

    Add missing LinkedHashMap import

--- app/src/main/java/com/example/new1/RoomContentActivity.java
+++ app/src/main/java/com/example/new1/RoomContentActivity.java
@@ -73,6 +73,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
